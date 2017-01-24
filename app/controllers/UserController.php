6d08168d49b767a20bbebd0efcb6d669791b6d54
jahng <ye.jia.hang@163.com>
2017-01-24 23:26:52 +0800

    update redis config

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -14,7 +14,7 @@ class UserController extends BaseController{
 		$info = $user_model->findAll();
 
 		// test case for RedisClient service
-		$this->container->get('RedisClient')->set('info', json_encode($info));
+		//$this->container->get('RedisClient')->set('info', json_encode($info));//annotated, since the redis is set to disabled in config.yml
 
 		$template = $this->twig->load('userinfo.html');
 		echo $template->render(array('userinfo' => $info));
